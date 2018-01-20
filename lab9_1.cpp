6ea5fcbd6a3b3d6d6c50e603ae472215226ff139
Unknown <Kan_30489@hotmail.com>
2018-01-20 14:17:25 +0700

    ใบงานที่9.2

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
 
 using namespace std;
+void swapnumber (int *x, int *y) {
+    int temp;
 
+    temp = *x;
+    *x = *y;
+    *y = temp;
+
+
+
+}
+void showValue (int x,int y) {
+    cout <<"This is x : " << x << endl;
+    cout <<"This is y : " << y << endl;
+}
 int main() {
     /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
     โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
     */
+
+
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    showValue (a, b);
+    int cmd;
+    do {
+
+    cin >> cmd;
+    if(cmd == 1){
+        swapnumber(&a, &b);
+        showValue(a, b);
+    }
+
+} while(cmd != 2);
+
+
 
 
 
